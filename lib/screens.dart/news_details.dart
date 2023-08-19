6bc97163f306afa284c92ecea8ba093fb74c9680
Ayush Rakesh <ayushrakesh35711artlover@gmail.com>
2023-08-19 13:56:29 +0530

    All News screen and detail news screen  updated

--- lib/screens.dart/news_details.dart
+++ lib/screens.dart/news_details.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
 import 'package:vids_com/widgets/chewei_item.dart';
@@ -41,20 +42,20 @@ class _NewsDetailsState extends State<NewsDetails> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('All news'),
+        title: const Text('News description'),
         leading: IconButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          icon: Icon(
+          icon: const Icon(
             Icons.arrow_back_ios,
           ),
         ),
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(
-          horizontal: width * 0.05,
-        ),
+            // horizontal: width * 0.05,
+            ),
         child: Column(
           children: [
             Container(
@@ -71,30 +72,119 @@ class _NewsDetailsState extends State<NewsDetails> {
                 true,
               ),
             ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: updateLikedStatus,
-                  icon: newsDetails!['is-liked']
-                      ? const Icon(
-                          Icons.thumb_up_alt_rounded,
-                        )
-                      : const Icon(
-                          Icons.thumb_up_outlined,
-                        ),
+            SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: width * 0.04,
+                  vertical: height * 0.01,
                 ),
-                IconButton(
-                  onPressed: updatedisLikedStatus,
-                  icon: newsDetails!['is-disliked']
-                      ? const Icon(
-                          Icons.thumb_down_alt_rounded,
-                        )
-                      : const Icon(
-                          Icons.thumb_down_alt_outlined,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.thumb_up_alt_rounded,
+                                ),
+                                Text(
+                                  newsDetails!['likes'].toString(),
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Gap(width * 0.05),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.thumb_down_alt_rounded,
+                                ),
+                                Text(
+                                  newsDetails!['dislikes'].toString(),
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
+                        Text(newsDetails!['date']),
+                        Row(
+                          children: [
+                            Text(newsDetails!['views'].toString()),
+                            Text(' views'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Gap(height * 0.02),
+                    Text(
+                      newsDetails!['title'],
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Gap(height * 0.01),
+                    Text(newsDetails!['description']),
+                    Gap(height * 0.025),
+                    const Text(
+                      'Comments',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemBuilder: (ctx, index) {
+                        return Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: width * 0.03,
+                            vertical: height * 0.01,
+                          ),
+                          margin: EdgeInsets.only(bottom: height * 0.01),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color.fromARGB(255, 42, 41, 41),
+                                Colors.black,
+                              ],
+                              end: Alignment.topLeft,
+                              begin: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                newsDetails!['comments'][index]['commented-by']
+                                    .toString(),
+                                style: const TextStyle(
+                                    fontSize: 18, color: Colors.white),
+                              ),
+                              Text(newsDetails!['comments'][index]['comment']
+                                  .toString()),
+                            ],
+                          ),
+                        );
+                      },
+                      itemCount: newsDetails!['comments'].length,
+                    )
+                  ],
                 ),
-              ],
-            )
+              ),
+            ),
           ],
         ),
       ),
